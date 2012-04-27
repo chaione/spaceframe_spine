16e5d481b93209b5ad0299c9b01dd9b911c61c4d
Scott Burton <scottburton11@gmail.com>
2012-04-27 07:35:14 -0700

    Rearrange the generator namespace a little, since we will have several; the new SpaceFrame::Spine::Base generator base class should be inherited from for new generators

--- lib/generators/spaceframe_new/spaceframe_new_generator.rb
+++
@@ -1,7 +0,0 @@
-class SpaceframeNewGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
-  
-  def create_empty_shell  
-    directory "app", "app/assets/javascripts/app"
-  end  
-end
