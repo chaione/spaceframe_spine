fa08b3a02199dedea2f2a2e9254f1e9ad0b4854c
Ryan Marsh <ryan.marsh@mac.com>
2012-04-27 02:50:36 -0500

    No longer requires a "name" option and copies spine directory structure to app/assets/javascripts/app

--- lib/generators/spaceframe_new/spaceframe_new_generator.rb
+++ lib/generators/spaceframe_new/spaceframe_new_generator.rb
@@ -1,3 +1,7 @@
-class SpaceframeNewGenerator < Rails::Generators::NamedBase
+class SpaceframeNewGenerator < Rails::Generators::Base
   source_root File.expand_path('../templates', __FILE__)
+  
+  def create_empty_shell  
+    directory "app", "app/assets/javascripts/app"
+  end  
 end
